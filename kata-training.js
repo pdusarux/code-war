9068a21a9122851673b72eaffcd2afb0233dfd26
ploydsr <p.dusarux@gmail.com>
2023-02-05 11:37:39 +0700

    String ends with?

--- kata-training.js
+++ kata-training.js
@@ -1,21 +1,21 @@
-function XO(str) {
-  let xCount = 0;
-  let oCount = 0;
-  let strArray = str.split("");
+/* Complete the solution so that it returns true if the first argument(string) 
+passed in ends with the 2nd argument (also a string).
+example:
+  solution('abc', 'bc') // returns true
+  solution('abc', 'd') // returns false
+*/
+
+function solution(str, ending) {
+  let endArray = ending.split("");
+  let strArray = str.split("").slice(endArray.length * -1);
+
   for (let i = 0; i < strArray.length; i++) {
-    if (strArray[i] === "x" || strArray[i] === "X") {
-      xCount++;
-    } else if (strArray[i] === "o" || strArray[i] === "O") {
-      oCount++;
+    if (strArray[i] !== endArray[i]) {
+      return false;
     }
   }
-  if (xCount === oCount) {
-    return true;
-  } else if (xCount === 0 && oCount === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 }
 
-console.log(XO("xoo"));
+console.log(solution("abcde", "cde"));
+console.log(solution("abcdef", "d"));
