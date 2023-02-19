f752f26fb1fe0b6dc895599be85ccb36ece2207a
ploydsr <p.dusarux@gmail.com>
2023-02-19 10:42:16 +0700

    Unique In Order

--- kata-training.js
+++ kata-training.js
@@ -1,40 +1,22 @@
 /*
-Counting Duplicates
+Unique In Order
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 */
 
-function duplicateCount(text) {
+var uniqueInOrder = function (iterable) {
+  let newValue = typeof iterable === "string" ? iterable.split("") : iterable;
   let newArr = [];
-  let newValue = text.toLowerCase().split("");
-  newValue.forEach((e) => newArr.push(newValue.indexOf(e)));
-  let count = 0;
-  let newArr2 = [];
 
-  for (let i = 0; i < newArr.length; i++) {
-    for (let j = 0; j < newArr.length; j++) {
-      if (newArr[i] === newArr[j]) {
-        count++;
-      }
-    }
-    if (count > 1) {
-      newArr2.push(newArr[i]);
-    }
-    count = 0;
+  for (let i = 0; i < newValue.length; i++) {
+    if (newValue[i] !== newValue[i - 1]) newArr.push(newValue[i]);
   }
-  let newArr3 = [...new Set(newArr2)];
 
-  return newArr3.length;
-}
+  return newArr;
+};
 
-// shortest solution by chatGPT
-// function duplicateCount(text) {
-//   let count = 0,
-//     arr = [...new Set(text.toLowerCase().split(""))];
-//   arr.forEach((char) => {
-//     console.log("char:>", char);
-//     console.log(text.toLowerCase().split(char));
-//     text.toLowerCase().split(char).length - 1 > 1 ? count++ : count;
-//   });
-//   return count;
-// }
-
-console.log(duplicateCount("aabBcccdef"));
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
