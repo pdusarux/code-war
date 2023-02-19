936ac96312dc0f0e7eade429b4ff60c237b74bf2
ploydsr <p.dusarux@gmail.com>
2023-02-19 10:10:45 +0700

    Counting Duplicates

--- kata-training.js
+++ kata-training.js
@@ -1,5 +1,40 @@
-function simpleMultiplication(number) {
-  return number % 2 == 0 ? number * 8 : number * 9;
+/*
+Counting Duplicates
+*/
+
+function duplicateCount(text) {
+  let newArr = [];
+  let newValue = text.toLowerCase().split("");
+  newValue.forEach((e) => newArr.push(newValue.indexOf(e)));
+  let count = 0;
+  let newArr2 = [];
+
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < newArr.length; j++) {
+      if (newArr[i] === newArr[j]) {
+        count++;
+      }
+    }
+    if (count > 1) {
+      newArr2.push(newArr[i]);
+    }
+    count = 0;
+  }
+  let newArr3 = [...new Set(newArr2)];
+
+  return newArr3.length;
 }
 
-console.log(simpleMultiplication(9));
+// shortest solution by chatGPT
+// function duplicateCount(text) {
+//   let count = 0,
+//     arr = [...new Set(text.toLowerCase().split(""))];
+//   arr.forEach((char) => {
+//     console.log("char:>", char);
+//     console.log(text.toLowerCase().split(char));
+//     text.toLowerCase().split(char).length - 1 > 1 ? count++ : count;
+//   });
+//   return count;
+// }
+
+console.log(duplicateCount("aabBcccdef"));
