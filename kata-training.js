a721cf93ea7fb9665d775c62b620bc1d9a6cb2d9
ploydsr <p.dusarux@gmail.com>
2023-02-20 09:14:33 +0700

    All Star Code Challenge #18

--- kata-training.js
+++ kata-training.js
@@ -1,22 +1,9 @@
 /*
-Unique In Order
-
-For example:
-
-uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
-uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
-uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+All Star Code Challenge #18
 */
 
-var uniqueInOrder = function (iterable) {
-  let newValue = typeof iterable === "string" ? iterable.split("") : iterable;
-  let newArr = [];
+function strCount(str, letter) {
+  return str.split(letter).length - 1;
+}
 
-  for (let i = 0; i < newValue.length; i++) {
-    if (newValue[i] !== newValue[i - 1]) newArr.push(newValue[i]);
-  }
-
-  return newArr;
-};
-
-console.log(uniqueInOrder([1, 2, 2, 3, 3]));
+console.log(strCount("Hello", "o"));
