8ce4062b7115821fc8ee62540365df355855cf84
ploydsr <p.dusarux@gmail.com>
2023-02-20 09:36:56 +0700

    Number of trailing zeros of N!

--- kata-training.js
+++ kata-training.js
@@ -1,9 +1,21 @@
 /*
-All Star Code Challenge #18
+Number of trailing zeros of N!
+
+Examples
+zeros(6) = 1
+# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+zeros(12) = 2
+# 12! = 479001600 --> 2 trailing zeros
 */
 
-function strCount(str, letter) {
-  return str.split(letter).length - 1;
+function zeros(n) {
+  let count = 0;
+  while (n >= 5) {
+    n = Math.floor(n / 5);
+    count += n;
+  }
+  return count;
 }
 
-console.log(strCount("Hello", "o"));
+console.log(zeros(1000));
